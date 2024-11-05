33656a0e49f519393b970f53b86cb34d64d199d2
sanbei101 <sanbei101@outlook.com>
2024-11-05 22:21:22 +0800

    使用匿名结构体删减代码

--- internal/controller/judge_controller.go
+++ internal/controller/judge_controller.go
@@ -7,24 +7,22 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type JudgeCodeRequest struct {
-	Language  string `json:"language"`
-	ProblemID int    `json:"problem_id"`
-	Code      string `json:"code"`
-}
-
 func JudgeCode(c *fiber.Ctx) error {
-	var res JudgeCodeRequest
-	if err := c.BodyParser(&res); err != nil {
+	payload := struct {
+		Language  string `json:"language"`
+		ProblemID int    `json:"problem_id"`
+		Code      string `json:"code"`
+	}{}
+	if err := c.BodyParser(&payload); err != nil {
 		utils.HandleError(c, err, "请求格式错误")
 	}
 
-	problemCases, err := utils.GetTestCases(res.ProblemID)
+	problemCases, err := utils.GetTestCases(payload.ProblemID)
 	if err != nil {
 		utils.HandleError(c, err, "获取测试用例失败")
 	}
 
-	evaluation, err := service.EvaluateProblem(res.Language, res.Code, problemCases)
+	evaluation, err := service.EvaluateProblem(payload.Language, payload.Code, problemCases)
 	if err != nil {
 		utils.HandleError(c, err, "评测失败")
 	}
