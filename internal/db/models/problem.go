2ce06b2232718ce735d9ca07a87264120f783e1c
sanbei101 <sanbei101@outlook.com>
2024-11-05 21:52:37 +0800

    修复不支持字符串数组的错误

--- internal/db/models/problem.go
+++ internal/db/models/problem.go
@@ -1,8 +1,12 @@
 package models
 
+import (
+	"github.com/lib/pq"
+)
+
 type Problems struct {
-	ID          int      `gorm:"column:id"`
-	Name        string   `gorm:"column:name"`
-	Description string   `gorm:"column:description"`
-	Tags        []string `gorm:"column:tags"`
+	ID          int            `gorm:"column:id"`
+	Name        string         `gorm:"column:name"`
+	Description string         `gorm:"column:description"`
+	Tags        pq.StringArray `gorm:"column:tags;type:text[]"`
 }
