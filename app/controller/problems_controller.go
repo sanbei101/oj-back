e6dd4650d87094fa48860ea668bb4e764d010c02
sanbei101 <sanbei101@outlook.com>
2024-11-06 19:14:33 +0800

    定义默认分页参数

--- app/controller/problems_controller.go
+++ app/controller/problems_controller.go
@@ -7,13 +7,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+const (
+	defaultPage = 1
+	defaultSize = 10
+)
+
 func GetAllProblems(c *fiber.Ctx) error {
-	page := c.QueryInt("page")
-	size := c.QueryInt("size")
-	keyword := c.Query("keyword")
+	page, size, keyword := c.QueryInt("page"), c.QueryInt("size"), c.Query("keyword")
 	if page == 0 || size == 0 {
-		page = 1
-		size = 10
+		page = defaultPage
+		size = defaultSize
 	}
 	problems, err := service.GetAllProblems(page, size, keyword)
 	if err != nil {
@@ -29,5 +32,6 @@ func GetProblemByID(c *fiber.Ctx) error {
 	if err != nil {
 		utils.HandleError(c, err, "获取题目失败")
 	}
+
 	return c.Status(fiber.StatusOK).JSON(problem)
 }
