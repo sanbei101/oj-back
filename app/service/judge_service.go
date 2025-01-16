f5006ac7f8667017e4aaeaa112646914d5ab5d98
sanbei101 <sanbei101@outlook.com>
2025-01-16 20:53:13 +0800

    feat: 将代码评测分类为解释型语言和编译型语言,模块化结构

--- app/service/judge_service.go
+++ app/service/judge_service.go
@@ -4,36 +4,50 @@ import (
 	"encoding/base64"
 	"fmt"
 	"oj-back/app/model"
-	"oj-back/pkg/utils"
+	"oj-back/pkg/judge"
+	"os"
+	"strings"
 	"sync"
 )
+
+var compiledLanguages = map[string]bool{
+	"c":    true,
+	"cpp":  true,
+	"java": true,
+	"go":   true,
+}
 
 type JudgeService struct{}
 
 var JudgeServiceApp = new(JudgeService)
 
+func CompareOutput(actualOutput string, expectedOutput string) (same bool, strictlySame bool) {
+	// TODO: 似乎逻辑有点不足,例如每一行行尾的/r/n或/n无法区分,需要改进一下.
+	actual := strings.TrimSpace(actualOutput)
+	expected := strings.TrimSpace(expectedOutput)
+	return actual == expected, expectedOutput == actualOutput
+}
+
 func (js *JudgeService) EvaluateProblem(language string, codeContent string, testCases []model.Case) (*model.EvaluationResult, error) {
-	var executablePath string
-	var compileErr error
+	// 客户端发送的代码是经过base64编码的
+	decodedCode, err := base64.StdEncoding.DecodeString(codeContent)
+
+	if err != nil {
+		return nil, fmt.Errorf("解码代码失败: %v", err)
+	}
 
 	// 针对编译型语言进行编译
-	if language == "c" {
-		// 解码代码
-		decodedCode, err := base64.StdEncoding.DecodeString(codeContent)
+	if compiledLanguages[language] {
+		executablePath, err := judge.CompiledJudgeApp.CompileCCode(decodedCode)
+		// 完成之后删除可执行文件
+		defer os.Remove(executablePath)
 		if err != nil {
-			return nil, fmt.Errorf("解码代码失败: %v", err)
-		}
-
-		// 编译C代码
-		executablePath, compileErr = utils.CompileCCode(decodedCode)
-		if compileErr != nil {
-			// 如果编译失败，所有测试用例均标记为编译错误
 			results := make([]model.TestResult, len(testCases))
 			for i := range results {
 				results[i] = model.TestResult{
 					IsSuccess:      false,
 					ExpectedOutput: testCases[i].ExpectedOutput,
-					ActualOutput:   compileErr.Error(),
+					ActualOutput:   err.Error(),
 				}
 			}
 			evaluation := &model.EvaluationResult{
@@ -42,55 +56,64 @@ func (js *JudgeService) EvaluateProblem(language string, codeContent string, tes
 			}
 			return evaluation, nil
 		}
-	}
 
-	var wg sync.WaitGroup
-	results := make([]model.TestResult, len(testCases))
-
-	for i, testCase := range testCases {
-		wg.Add(1)
-		go func(i int, tc model.Case) {
-			defer wg.Done()
-
-			var output string
-			var err error
-
-			if language == "c" {
+		// 并发执行测试用例
+		var wg sync.WaitGroup
+		results := make([]model.TestResult, len(testCases))
+		wg.Add(len(testCases))
+		for i, testCase := range testCases {
+			go func(i int, tc model.Case) {
+				defer wg.Done()
 				// 对编译型语言语言，运行已编译的可执行文件
-				output, err = utils.SubmitExecutableTask(executablePath, tc.Input)
-			} else if language == "python" {
-				// 对于解释型语言，使用现有的RunCode函数
-				output, err = utils.SubmitCodeExecution(language, codeContent, tc.Input)
-			} else {
-				err = fmt.Errorf("不支持的语言")
-			}
-
-			if err != nil {
-				results[i] = model.TestResult{
-					IsSuccess:      false,
-					ExpectedOutput: tc.ExpectedOutput,
-					ActualOutput:   err.Error(),
+				output, err := judge.CompiledJudgeApp.SubmitJudge(executablePath, tc.Input)
+				if err != nil {
+					output = err.Error()
 				}
-				return
-			}
+				isCorrect, isStrictlyCorrect := CompareOutput(output, tc.ExpectedOutput)
+				results[i] = model.TestResult{
+					IsSuccess:       isCorrect,
+					IsStrictSuccess: isStrictlyCorrect,
+					ExpectedOutput:  tc.ExpectedOutput,
+					ActualOutput:    output,
+				}
+			}(i, testCase)
+		}
 
-			// 比较输出
-			isCorrect, isStrictlyCorrect := utils.CompareOutput(output, tc.ExpectedOutput)
-			results[i] = model.TestResult{
-				IsSuccess:       isCorrect,
-				IsStrictSuccess: isStrictlyCorrect,
-				ExpectedOutput:  tc.ExpectedOutput,
-				ActualOutput:    output,
-			}
-		}(i, testCase)
+		wg.Wait()
+		evaluation := &model.EvaluationResult{
+			Count:   len(results),
+			Results: results,
+		}
+
+		return evaluation, nil
+	} else {
+		// 对解释型语言，直接运行代码
+		var wg sync.WaitGroup
+		results := make([]model.TestResult, len(testCases))
+		wg.Add(len(testCases))
+		for i, testCase := range testCases {
+			go func(i int, tc model.Case) {
+				defer wg.Done()
+				output, err := judge.InterpretedJudgeApp.SubmitJudge(language, decodedCode, tc.Input)
+				if err != nil {
+					output = err.Error()
+				}
+				isCorrect, isStrictlyCorrect := CompareOutput(output, tc.ExpectedOutput)
+				results[i] = model.TestResult{
+					IsSuccess:       isCorrect,
+					IsStrictSuccess: isStrictlyCorrect,
+					ExpectedOutput:  tc.ExpectedOutput,
+					ActualOutput:    output,
+				}
+			}(i, testCase)
+		}
+
+		wg.Wait()
+		evaluation := &model.EvaluationResult{
+			Count:   len(results),
+			Results: results,
+		}
+
+		return evaluation, nil
 	}
-
-	wg.Wait()
-
-	evaluation := &model.EvaluationResult{
-		Count:   len(results),
-		Results: results,
-	}
-
-	return evaluation, nil
 }
